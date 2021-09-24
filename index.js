de6d36fa2210739d54af2cda5780eb489642f201
Yavuz <88813478+yavuzinc@users.noreply.github.com>
2021-09-24 17:35:15 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function () {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+
+const task1 = newTask("Clean Cat Litter", "Take all the poop out of the litter box");
+const task2 = newTask ("Do Laundry", "Boo");
+const tasks = [task1, task2];
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
